2f2f33c61eacd944f51274ac944a1966e1a37056
chintan mistry <chintanmistry1307@gmail.com>
2022-02-21 12:52:57 +0530

    commit

--- lib/screens/welcomescreen.dart
+++ lib/screens/welcomescreen.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mr_cafe/screens/login_screen.dart';
-import 'package:splashscreen/splashscreen.dart';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  
-  const WelcomeScreen({ Key? key }) : super(key: key);
+  const WelcomeScreen({Key? key}) : super(key: key);
   static const String id = 'welcome_screen';
 
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      backgroundColor: Colors.white,
-      navigateAfterSeconds: LoginPage(),
-      title:  Text('ABC',textScaleFactor: 2,),
-      image:  Image(image: AssetImage('assets/logo.png'),),
-      loadingText: const Text("Loading"),
-      photoSize: 100.0,
-      loaderColor: Colors.black54,
+    return Container(
+      color: Colors.blueGrey,
     );
   }
 }
