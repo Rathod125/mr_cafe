29443ec03d34e719c560345309e264478652a02d
rathod125 <tusharrathod5801@gmail.com>
2022-02-23 15:25:56 +0530

    commit

--- lib/widgets/toppicks.dart
+++ lib/widgets/toppicks.dart
@@ -5,90 +5,73 @@ class Toppickes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
       children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(right: 5),
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Color(0xFF212325),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: CircleAvatar(
-                      radius: 60,
-                      backgroundImage: AssetImage('assets/cofee.jpg'),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Espresso \nCapuccino",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Libre Baskerville',
-                        color: Color(0xFFEADBCC)),
-                  ),
-                  Text(
-                    'Dark Roast',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ToppicksCard(),
+            ToppicksCard(),
+          ],
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-              decoration: BoxDecoration(
-                  color: Color(0xFF212325),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: CircleAvatar(
-                      radius: 60,
-                      backgroundImage: AssetImage('assets/cofee.jpg'),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Espresso \nCapuccino",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Libre Baskerville',
-                        color: Color(0xFFEADBCC)),
-                  ),
-                  Text(
-                    'Dark Roast',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ToppicksCard(),
+            ToppicksCard(),
+          ],
         ),
       ],
+    );
+  }
+}
+
+class ToppicksCard extends StatelessWidget {
+  const ToppicksCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 5, top: 5),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          decoration: BoxDecoration(
+              color: Color(0xFF212325),
+              borderRadius: BorderRadius.circular(20)),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/cofee.jpg'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Espresso \nCapuccino",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Libre Baskerville',
+                    color: Color(0xFFEADBCC)),
+              ),
+              Text(
+                'Dark Roast',
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
