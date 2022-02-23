52bffb4e58dac2e041cee035c22bb0f614f014b0
rathod125 <tusharrathod5801@gmail.com>
2022-02-23 11:08:02 +0530

    commit

--- lib/widgets/catagory_gridview.dart
+++ lib/widgets/catagory_gridview.dart
@@ -11,79 +11,61 @@ class CategoriesList extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
-          child: Column(children: const [
-            CircleAvatar(
-              backgroundColor: Color(0xFF212325),
-              radius: 42,
-              child: CircleAvatar(
-                backgroundColor: Color(0xFFEADBCC),
-                radius: 40,
-                child: Image(
-                  image: AssetImage(
-                    'assets/logo.png',
-                  ),
-                  // height: 100,
-                  // width: 100,
-                ),
-              ),
-            ),
-            Text(
-              "capaccino",
-              maxLines: 1,
-              style: TextStyle(fontSize: 20, color: Color(0xFF212325)),
-            ),
-          ]),
-        ),
-        const Expanded(
-          child: ListTile(
-            title: CircleAvatar(
-              backgroundColor: Color(0xFF212325),
-              radius: 42,
-              child: CircleAvatar(
-                backgroundColor: Color(0xFFEADBCC),
-                radius: 40,
-                child: Image(
-                  image: AssetImage(
-                    'assets/logo.png',
-                  ),
-                  // height: 100,
-                  // width: 100,
-                ),
-              ),
-            ),
-            subtitle: Text(
-              "capaccino",
-              style: TextStyle(fontSize: 20, color: Color(0xFF212325)),
-            ),
+          child: CategoryLogo(
+            cname: 'capaccino',
+            onpress: () {
+              Navigator.pushNamed(context, Categories.id);
+            },
           ),
         ),
         Expanded(
-          child: ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, Categories.id);
-            },
-            title: CircleAvatar(
-              backgroundColor: Color(0xFF212325),
-              radius: 42,
-              child: CircleAvatar(
-                backgroundColor: Color(0xFFEADBCC),
-                radius: 40,
-                child: Image(
-                  image: AssetImage(
-                    'assets/logo.png',
-                  ),
-                  // height: 100,
-                  // width: 100,
-                ),
-              ),
-            ),
-            subtitle: Text(
-              "capaccino",
-              style: TextStyle(fontSize: 20, color: Color(0xFF212325)),
-            ),
+          child: CategoryLogo(
+            cname: 'Tea',
+            onpress: () {},
+          ),
+        ),
+        Expanded(
+          child: CategoryLogo(
+            cname: 'Desserts',
+            onpress: () {},
           ),
         ),
       ],
+    );
+  }
+}
+
+class CategoryLogo extends StatelessWidget {
+  CategoryLogo({required this.cname, required this.onpress});
+  String cname;
+  VoidCallback onpress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onpress,
+      child: Column(children: [
+        const CircleAvatar(
+          backgroundColor: Color(0xFF212325),
+          radius: 42,
+          child: CircleAvatar(
+            backgroundColor: Color(0xFFEADBCC),
+            radius: 40,
+            child: Image(
+              image: AssetImage(
+                'assets/logo.png',
+              ),
+              // height: 100,
+              // width: 100,
+            ),
+          ),
+        ),
+        Text(
+          cname,
+          maxLines: 1,
+          style: TextStyle(fontSize: 20, color: Color(0xFF212325)),
+        ),
+      ]),
     );
   }
 }
