32b052420e51bfef1f0abe8cdb2086660119965b
Darshil3099 <darshilkanani3099@gmail.com>
2022-02-23 11:33:06 +0530

    commit

--- lib/widgets/itemlist.dart
+++ lib/widgets/itemlist.dart
@@ -5,61 +5,9 @@ class Itemlist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            color: Colors.white70),
-        child: Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-
-            // SkeletonAnimation method
-            children: <Widget>[
-              Container(
-                width: 70.0,
-                height: 70.0,
-                child: Image.asset('assets/cofee.jpg'),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0, bottom: 5.0),
-                      child: Container(
-                        height: 15,
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.grey[300]),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 5.0),
-                        child: Container(
-                          width: 60,
-                          height: 13,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              color: Colors.grey[300]),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(child: Text('346'))
-            ],
-          ),
-        ),
+    return SafeArea(
+      child: Column(
+        children: [ItemCard(), ItemCard()],
       ),
     );
 
@@ -94,5 +42,73 @@ class Itemlist extends StatelessWidget {
     //     )
     //   ],
     // );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            color: Colors.white70),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+
+          // SkeletonAnimation method
+          children: <Widget>[
+            Container(
+              width: 70.0,
+              height: 70.0,
+              child: Image.asset('assets/cofee.jpg'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0, bottom: 5.0),
+                    child: Container(
+                      height: 15,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: Container(
+                        width: 60,
+                        height: 13,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.grey[300]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Text(
+                '346/',
+                textAlign: TextAlign.end,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
