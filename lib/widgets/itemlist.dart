46e087d2e508250a35255721a3470ad115fe2a95
Darshil3099 <darshilkanani3099@gmail.com>
2022-02-23 16:48:59 +0530

    commit

--- lib/widgets/itemlist.dart
+++ lib/widgets/itemlist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mr_cafe/screens/item.dart';
 
 class Itemlist extends StatelessWidget {
   const Itemlist({Key? key}) : super(key: key);
@@ -7,41 +8,15 @@ class Itemlist extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
-        children: [ItemCard(), ItemCard()],
+        children: const [
+          SizedBox(
+            height: 10,
+          ),
+          ItemCard(),
+          ItemCard()
+        ],
       ),
     );
-
-    //  Column(
-    //   children: [
-    //     Container(
-    //       decoration: const BoxDecoration(color: Colors.red),
-    //       height: 300,
-    //       child: const ListTile(
-    //         leading: CircleAvatar(
-    //           backgroundImage: AssetImage('assets/logo.png'),
-    //           radius: 30,
-    //           backgroundColor: Colors.white,
-    //         ),
-    //         title: Text('Espresso'),
-    //         subtitle: Text('coffee'),
-    //         trailing: Text('95/-'),
-    //       ),
-    //     ),
-    //     Container(
-    //       height: 100,
-    //       child: const ListTile(
-    //         tileColor: Colors.brown,
-    //         leading: Image(
-    //           image: AssetImage('assets/cofee.jpg'),
-    //           height: 100,
-    //           width: 100,
-    //         ),
-    //         title: Text('Espresso'),
-    //         trailing: Text('95/-'),
-    //       ),
-    //     )
-    //   ],
-    // );
   }
 }
 
@@ -52,72 +27,99 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 100,
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            color: Color(0xFF212325)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-
-          // SkeletonAnimation method
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              height: 100.0,
-              child: Image.asset(
-                'assets/cofee.jpg',
-                fit: BoxFit.fill,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (contex) {
+                return Item(itemname: 'Espresso');
+              }));
+            },
+            child: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Color(0xFF212325)),
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+
+                // SkeletonAnimation method
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, bottom: 5.0),
-                    child: Container(
-                      height: 25,
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      child: Text(
-                        'Espresso',
-                        style: TextStyle(fontSize: 20, color: Colors.white70),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0),
+                  Container(
+                    width: 110.0,
+                    height: 110.0,
                     child: Padding(
-                      padding: const EdgeInsets.only(right: 5.0),
-                      child: Container(
-                        width: 60,
-                        height: 13,
-                        child: Text(
-                          '150/-',
-                          style: TextStyle(color: Colors.white70),
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          'assets/cofee.jpg',
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 15.0, bottom: 5.0),
+                          child: Container(
+                            height: 25,
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: const Text(
+                              'Espresso',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white70,
+                                  fontFamily: 'Libre Baskerville'),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 5.0),
+                            child: Container(
+                              width: 60,
+                              height: 13,
+                              child: const Text(
+                                '150/-',
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Expanded(
+                      child: Icon(
+                    Icons.chevron_right,
+                    color: Colors.white70,
+                    size: 40,
+                  ))
                 ],
               ),
             ),
-            Expanded(
-                child: Icon(
-              Icons.chevron_right,
-              color: Colors.white70,
-              size: 40,
-            ))
-          ],
+          ),
         ),
-      ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Divider(
+            color: Colors.black45,
+            thickness: 0.5,
+          ),
+        ),
+      ],
     );
   }
 }
