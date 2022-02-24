ed002f62b8c93eef32fca72bda2d6b5d11ce8df0
rathod125 <tusharrathod5801@gmail.com>
2022-02-24 12:00:47 +0530

    commit

--- lib/screens/item.dart
+++ lib/screens/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Item extends StatelessWidget {
   Item({required this.itemname});
@@ -6,16 +7,36 @@ class Item extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFEADBCC),
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Color(0xFF212325)),
-        backgroundColor: Color(0xFFEADBCC),
-        title: Text(
-          itemname,
-          style: TextStyle(color: Color(0xFF212325)),
-        ),
-      ),
+    return ScreenUtilInit(
+      designSize: Size(700, 700),
+      builder: () {
+        return Scaffold(
+          backgroundColor: Color(0xFFEADBCC),
+          appBar: AppBar(
+            iconTheme: IconThemeData(color: Color(0xFF212325)),
+            backgroundColor: Color(0xFFEADBCC),
+            title: Text(
+              itemname,
+              style: TextStyle(color: Color(0xFF212325)),
+            ),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/coldbrew.jpeg'),
+                    radius: 150.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
